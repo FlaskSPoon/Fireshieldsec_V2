c4f2cc1b4c69ed58af2efc42d349d99833f6d5be
Seny BALDE <seny.balde1@unchk.edu.sn>
2025-06-09 23:34:07 -1200

    update

--- app/(services)/certification/page.jsx
+++ app/(services)/certification/page.jsx
@@ -120,8 +120,7 @@ export  function Specialisation(){
             {pricingPlans.map((card, index) => (
               <div className="pricing-card style3" key={index}>
                 <div className="pricing-card-header">
-                  {/* <div className="pricing-card-header_price">{card.price}</div> */}
-                  {/* <div className="pricing-card-header_text">{card.period}</div> */}
+                
                 </div>
                 <div
                   className="pricing-card-header_badge "
@@ -130,9 +129,7 @@ export  function Specialisation(){
                 >
                   <span>{card.badgeText}</span>
                 </div>
-                {/* <p className="text p-3">
-                Le plan tarifaire de base est conçu pour les individus ou les petites équipes qui sont{" "}
-                </p> */}
+                
                 <div className="checklist p-3">
                   {card.features.map((feature, featureIndex) => (
                     <ul key={featureIndex}>
@@ -249,65 +246,3 @@ export  function Specialisation(){
     )
    
 }
-
-{/* <div className="d-flex justify-content-between align-items-start mt-5">
-  
-
-<div className="w-50 card bg-light">
-  <h5 className="text-danger card text-center">FORMATIONS SPÉCIALISÉES</h5>
-  <ul className="text-dark">
-    <li>➤ FSEC-INT1 : Techniques essentielles de cybersécurité</li>
-    <li>➤ FSEC-INT2 : Fondamentaux techniques de la cybersécurité</li>
-    <li>➤ FSEC-IND : Cybersécurité des systèmes industriels</li>
-    <li>➤ FSEC-IoT : Sécurité des objets connectés</li>
-    <li>➤ FSEC-MOB : Audit sécurité d’applications mobiles Android et iOS</li>
-    <li>➤ FSEC-PKI : Principes et mise en œuvre des PKI</li>
-    <li>➤ FSEC-INC1 : Surveillance, détection et réponse aux incidents de sécurité</li>
-    <li>➤ FSEC-INC2 : Surveillance, détection et réponse aux incidents avancée</li>
-    <li>➤ FCySOC : Détection des incidents de sécurité</li>
-    <li>➤ FSEC-AF2 : Analyse forensique avancée</li>
-    <li>➤ C-AF1 : Analyse inforensique Windows</li>
-    <li>➤ FSEC-iPENT : Tests d’intrusion des systèmes industriels</li>
-    <li>➤ FSEC-WPEN : Test d’intrusion des serveurs et applications Web</li>
-    <li>➤ FSEC-PENT1 : Tests d’intrusion</li>
-    <li>➤ FSEC-PENT2 : Tests d’intrusion et développement d’exploits</li>
-    
-  </ul>
-  <p className=" d-flex justify-content-center"><Link scroll={false} href={`/E-learning`} className="gt-btn gt-btn-icon justify-center flex flex-col items-center ">
-            En savoir +
-          </Link></p>
-</div>
-
-<div className="w-50 bg-light p-4 rounded shadow">
-  <h5 className="text-center text-dark bg-danger p-2 rounded">INSCRIPTION</h5>
-  <form>
-    <div className="mb-3">
-      <label className="form-label" required>Nom & Prénom</label>
-      <input type="text" className="form-control" placeholder="Votre Nom" />
-    </div>
-    <div className="mb-3">
-      <label className="form-label" required>Email</label>
-      <input type="email" className="form-control" placeholder="Votre Email" />
-    </div>
-    <div className="mb-3">
-      <label className="form-label" required>Téléphone</label>
-      <input type="tel" className="form-control" placeholder="Votre Téléphone" />
-    </div>
-    <div className="mb-3">
-      <label className="form-label" required>Formation</label>
-      <select className="form-select">
-        <option value="" required>Sélectionnez la formation</option>
-        <option>FSEC-INT1</option>
-        <option>FSEC-INT2</option>
-        <option>FSEC-IND</option>
-        <option>FSEC-IoT</option>
-        <option>FSEC-MOB</option>
-        <option>FSEC-PKI</option>
-        <option> FSEC-PENT1</option>
-        <option>FSEC-WEB</option>
-      </select>
-    </div>
-    <button className="btn btn-danger w-100" onClick={handleClick}>S'inscrire</button>
-  </form>
-</div>
-</div> */}
