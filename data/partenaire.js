6d437749fd3a4a6c50dd7e6a3b2f07a66e3f5251
Seny BALDE <seny.balde1@unchk.edu.sn>
2025-06-10 23:03:05 -1200

    updated page partenaire

--- data/partenaire.js
+++ data/partenaire.js
@@ -2,55 +2,61 @@ export const parternCards = [
   {
     id: 29,
     thumbSrc: "/assets/img/service/serviceThumb2_1.png",
-    link: "/https://account.microsoft.com/",
+    link: "https://www.microsoft.com/",
     delay: ".3s",
     hasRipple: true
   },
   {
     id: 30,
     thumbSrc: "/assets/img/service/serviceThumb2_2.png",
-    link: "/https://www.dell.com/",
+    link: "https://www.dell.com/",
     delay: ".6s"
   },
   {
     id: 31,
     thumbSrc: "/assets/img/service/serviceThumb2_3.png",
-   
-    link: "/https://www.comptia.org/",
+    link: "https://partners.comptia.org/",
     delay: ".9s"
   },
   {
     id: 32,
     thumbSrc: "/assets/img/service/serviceThumb2_4.png",
-    link: "/https://pecb.com",
+    link: "https://pecb.com/",
     delay: "1.2s"
   },
   {
     id: 33,
     thumbSrc: "/assets/img/service/serviceThumb2_5.png",
-    link: "/https://www.ibm.com/",
+    link: "https://www.ibm.com/",
     delay: "1.3s",
     hasRipple: true
   },
   {
     id: 34,
     thumbSrc: "/assets/img/service/serviceThumb2_6.png",
-    link: "/https://www.fortinet.com/",
+    link: "https://www.fortinet.com/",
     delay: "1.5s"
   },
   {
     id: 35,
     thumbSrc: "/assets/img/service/serviceThumb2_7.png",
-    link: "/https://www.veeam.com/",
+    link: "https://www.veeam.com/",
     delay: "1.7s"
   },
   {
     id: 36,
     thumbSrc: "/assets/img/service/serviceThumb2_8.png",
-    link: "/https://info.knowbe4.com/",
+    link: "https://www.knowbe4.com/",
+    delay: "1.9s"
+  },
+  {
+    id: 36,
+    thumbSrc: "/assets/img/service/tenable.png",
+    link: "https://fr.tenable.com/",
     delay: "1.9s"
   }
 ];
 [
   {}
 ]
+
